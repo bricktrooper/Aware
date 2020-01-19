f3c73e94a304830b22694c8961f297eeb9447fa3
shusilshapkota <shusilshapkota@gmail.com>
2020-01-19 09:33:47 -0500

    Switched from SwiftUI to storyBoard

---
+++ AwareApp/Aware/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Aware
+//
+//  Created by Shusil Shapkota on 2020-01-19.
+//  Copyright © 2020 Shusil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
