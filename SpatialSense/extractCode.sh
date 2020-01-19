d38a3b0d4d006c1b2be716d9813c418727d61b94
bricktrooper <kylepinto@yahoo.ca>
2020-01-19 02:03:18 -0500

    Improve extract script

--- SpatialSense/extractCode.sh
+++ SpatialSense/extractCode.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-rm -rf code
-tar -xvf code.tar
-rm code.tar
+test -e code.tar
+
+if [[ $? -ne 0 ]]; then
+	echo "no tar file, aborting"
+else
+	rm -rf code
+	tar -xvf code.tar
+	rm code.tar
+fi
